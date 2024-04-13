2866978ce15a8d52f93373e376bee56ca081d49d
刘雅琴 <liuyaqin@liuyaqindeMacBook-Pro.local>
2024-04-13 15:28:16 +0200

    remove old frontend files

--- Eventpro/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material'
-
-export default () => {
-  return (
-    <Box sx={{ mt: 10, mb: 5 }}>
-      <span style={{ color: '#6F767E' }}>© 2024 EventPro</span>
-    </Box>
-  )
-}
--- Eventpro/src/store/index.js
+++
@@ -1,6 +0,0 @@
-export { observer } from 'mobx-react-lite'
-import walletStore from './wallet'
-import marketStore from './market'
-import modalStore from './modal'
-
-export { walletStore, marketStore, modalStore }
