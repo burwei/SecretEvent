b9750de86435cbe48b7de75d60c4fe44fe938fd8
burwei <thisisburwei@gmail.com>
2024-04-13 19:54:38 +0200

    change camera to qr scanner, it works

--- scaffolds/packages/nextjs/components/cemera.tsx
+++ scaffolds/packages/nextjs/components/cemera.tsx
@@ -1,31 +1,12 @@
-import React, { useRef, useEffect } from 'react';
+import { Scanner } from '@yudiel/react-qr-scanner';
 
-const Camera: React.FC = () => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-
-    useEffect(() => {
-        // Request access to the camera
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            const constraints: MediaStreamConstraints = {
-                video: true
-            };
-
-            // Access the camera
-            navigator.mediaDevices.getUserMedia(constraints)
-                .then((stream) => {
-                    // Attach the video stream to the video element and autoplay
-                    if (videoRef.current) {
-                        videoRef.current.srcObject = stream;
-                        videoRef.current.play();
-                    }
-                })
-                .catch((error) => {
-                    console.error("Error accessing the media devices.", error);
-                });
-        }
-    }, []);
-
-    return <video ref={videoRef} />;
-};
+const Camera = () => {
+    return (
+        <Scanner
+            onResult={(text: string, _) => console.log(text)}
+            onError={(error) => console.log(error?.message)}
+        />
+    );
+}
 
 export default Camera;
